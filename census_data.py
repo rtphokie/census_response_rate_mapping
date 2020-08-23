c7390bc26bf6a3bda264dac8b2d011a92c21b3c8
trice <trice@cisco.com>
2020-08-23 17:26:11 -0400

    calculate bounding boxes from state and county list

--- census_data.py
+++ census_data.py
@@ -1,20 +1,25 @@
 import requests
+from os import path
+import pandas as pd
+import numpy as np
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 key="1686d53efc1d5b59c22bc7c54b7c755e88653a7a"
 APIURLBASE='https://api.census.gov/data/2020/dec'
 
 def get_response_by_county(county='*', state='37', tract='*',
-                          PARAMSTOFETCH='GEO_ID,NAME,CRRALL,DRRALL,CRRINT,DRRINT,RESP_DATE'):
+                          PARAMSTOFETCH='GEO_ID,NAME,CRRALL,CRRINT,RESP_DATE'):
     return _get_response(county=county, state=state, tract=tract, datares='county',
                          PARAMSTOFETCH=PARAMSTOFETCH)
 
 def get_response_by_tract(county='*', state='37', tract='*',
-                          PARAMSTOFETCH='GEO_ID,NAME,CRRALL,DRRALL,CRRINT,DRRINT,RESP_DATE'):
+                          PARAMSTOFETCH='GEO_ID,NAME,CRRALL,CRRINT,RESP_DATE'):
     return _get_response(county=county, state=state, tract=tract, datares='tract',
                          PARAMSTOFETCH=PARAMSTOFETCH)
 
 def _get_response(county='*', state='37', tract='*', datares='tract',
-                  PARAMSTOFETCH='GEO_ID,NAME,CRRALL,DRRALL,CRRINT,DRRINT,RESP_DATE'):
+                  PARAMSTOFETCH='GEO_ID,NAME,CRRALL,CRRINT,RESP_DATE'):
     url = f"{APIURLBASE}/responserate?get={PARAMSTOFETCH}&for={datares}:*&in=state:{state}&key={key}"
     r = requests.get(url)
     data = r.json()
@@ -27,3 +32,32 @@ def _get_response(county='*', state='37', tract='*', datares='tract',
         else:
             result[res['county']] = res
     return result
+
+
+def get_bounding_box(state=None, counties=[]):
+    '''
+    
+    :param state: optional state FIPS code to filter by (e.g. NC=37)
+    :param counties: optional list of county FIPS codes to filter by (e.g. NC Wake = 183)
+    :return: 
+    '''
+    filename = 'bounding.csv'
+    if not path.exists(filename):
+        url = 'https://raw.githubusercontent.com/stucka/us-county-bounding-boxes/master/bounding.csv'
+        r = requests.get(url, allow_redirects=True)
+        open(filename, 'wb').write(r.content)
+
+    df = pd.read_csv(filename)
+    if state is not None:
+        df.query('statefips == 37', inplace=True)
+    if counties:
+        atoms = []
+        for county in counties:
+            atoms.append(f"countyfips == {county}")
+        df.query(" or ".join(atoms), inplace = True)
+
+    bounds = {'llcrnrlon': df['extentw'].min(), 'llcrnrlat': df['extents'].min(),
+              'urcrnrlon':df['extente'].max(), 'urcrnrlat': df['extentn'].max(),
+              'lon_0': np.mean([df['extentw'].min(),df['extente'].min()])
+              }
+    return df, bounds
--- testthis.py
+++ testthis.py
@@ -1,15 +1,26 @@
 author = "@rtphokie"
 import unittest
 import matplotlib.pyplot as plt
+from pprint import pprint
 import requests_cache
-from census_response_rate import make_map, draw_tract_data, draw_county_data
-from census_data import get_response_by_county, get_response_by_tract
-
+import pandas as pd
+from census_response_rate_mapping import make_map, draw_tract_data, draw_county_data
+from census_data import get_response_by_county, get_response_by_tract, get_bounding_box
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 requests_cache.install_cache('census_cache')
 
 
 class MyTestCase(unittest.TestCase):
+
+    def test_get_bounding_box_data(self):
+        df, data = get_bounding_box()
+        self.assertEqual(len(df), 3221)
+        df, data = get_bounding_box(state=37)
+        self.assertEqual(len(df), 100)
+        df, data = get_bounding_box(state=37, counties=[183, 63])
+        self.assertEqual(len(df), 2)
 
     def test_census_by_county(self):
         data = get_response_by_county()
@@ -19,23 +30,41 @@ class MyTestCase(unittest.TestCase):
         data = get_response_by_tract()
         self.assertGreaterEqual(len(data), 1750)
 
-    def test_NC_tracts(self):
-        bounds = {'llcrnrlon': -84.6, 'llcrnrlat': 33.5,
-                  'urcrnrlon': -75.2, 'urcrnrlat': 37.0,
-                  'lon_0': -80.0,
-                  }
+    def test_map_central_NC_tracts(self):
+        countylist=[183,63, 37, 51, 65, 69, 77, 83, 85, 93, 101, 105, 127, 131, 135, 145, 163, 181, 185, 191, 195]
+        df, bounds = get_bounding_box(state=37, counties=countylist)
         ax, fig, map = make_map(bounds)
-        draw_tract_data(ax, map, fig)
+        draw_tract_data(ax, map, fig, counties=countylist)
         plt.show()
+        fig.savefig('census_NC_tracts.png')
 
-    def test_NC_counties(self):
-        bounds = {'llcrnrlon': -84.6, 'llcrnrlat': 33.5,
-                  'urcrnrlon': -75.2, 'urcrnrlat': 37.0,
-                  'lon_0': -80.0,
-                  }
+    def test_map_NC_tracts(self):
+        df, bounds = get_bounding_box(state=37)
+
+        ax, fig, map = make_map(bounds)
+        data = draw_tract_data(ax, map, fig)
+        df = pd.DataFrame(data.values())
+        df.to_excel("NC_tracts.xlsx")
+
+        plt.show()
+        fig.savefig('census_NC_tracts.png')
+
+    def test_map_NC_counties(self):
+        df, bounds = get_bounding_box(state=37)
+
         ax, fig, map = make_map(bounds)
         draw_county_data(ax, map, fig)
         plt.show()
+        fig.savefig('census_NC_counties.png')
 
+    def test_map_central_NC_counties(self):
+        countylist=[183,63, 37, 51, 65, 69, 77, 83, 85, 93, 101, 105, 127, 131, 135, 145, 163, 181, 185, 191, 195]
+        df, bounds = get_bounding_box(state=37, counties=countylist)
+        ax, fig, map = make_map(bounds)
+        draw_tract_data(ax, map, fig, counties=countylist)
+        plt.show()
+        fig.savefig('census_NC_tracts.png')
 if __name__ == '__main__':
     unittest.main()
+
+
